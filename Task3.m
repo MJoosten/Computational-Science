63742f7a46eb85f7797355e8784db6d2015b4992
sandcrawler <nemoandrea@gmail.com>
2017-06-30 16:29:27 +0200

    Task 3 Initial Vectorisation
    Now function for current initial vector. Todo: for any initial vector; remove N for loop

--- Task3.m
+++ Task3.m
@@ -13,18 +13,15 @@ format compact;
 
 %% Start
 
-%----------------------TODO: VECTORISE, BABY ------------------------------
 %parameters
 enable_plots=true; %do you wish to plot the WLC? 
 enable_debug_plots=false; %do you wish to view debugging plots? 
 enable_error_plot=true;
 plot_scatter=true; %do you wish to havea  scatter plot over plot3 for 
                    %the plot with multiple WLC in one figure?
-plot_wlc_number=10; %how many WLC do you wish to plot in one single figure?
-                    %requires enable_plots to be true to have effect
 P=8; %number of configurations (configuration = amount segments of chain) 
-P_range=[10,5000]; %range of segment numbers (default: 100,5000)
-N=100; %Iterations of Polymer/chain (DNA) generation (default:100)
+P_range=[50,10000]; %range of segment numbers (default: 100,5000)
+N=1000; %Iterations of Polymer/chain (DNA) generation (default:100)
 K=round(linspace(P_range(1),P_range(2),P)); % Number of segments of chain
                                      %(base pairs) (default:2000)
 length_link=0.311;%[nm] Length of each chain link(base pair)(default:0.311)
@@ -43,44 +40,41 @@ distances=zeros(N,P); %will hold the squared end-to-end distances
 fprintf(['\n>>>[task 3] Starting Computation WLC 3D with %u'...
         ' configurations each with %u iterations and number of segments '...
         'between %u and %u'],P,N,min(K),max(K))
+
 for pp=1:P
     K_local=K(pp);
     
     %Preallocation - Inside Loop
     location=zeros(3,K_local,N); %will hold the location for each polymer link (3D)
     tangents=ones(3,K_local,N);% holds the angles %TODO: do this more efficiently
-    tangents(1,:,:)=tangents(1,:,:)*t_initial(1); %setting initial tangent
-    tangents(2,:,:)=tangents(2,:,:)*t_initial(2); %setting initial tangent
-    tangents(3,:,:)=tangents(3,:,:)*t_initial(3); %setting initial tangent
 
     % generate random bend angles
     % Gaussian Distribution with mu=0;var=length_link/length_persistence
-    rand_angles=sqrt(length_link/length_persist)*randn(2,K_local,N);
+    rand_angles1=sqrt(length_link/length_persist)*randn(K_local,N);
+    rand_angles2=sqrt(length_link/length_persist)*randn(K_local,N);  
+    %Cumulative Angles (these are the random angles values we will rotate at)
+    cum_angles1=cumsum(rand_angles1,1);
+    cum_angles2=cumsum(rand_angles2,1);
+    
+    %compute cosines and sines beforehand
+    cos_test_1=cos(cum_angles1);
+    sin_test_1=sin(cum_angles1);
+    cos_test_2=cos(cum_angles2);
+    sin_test_2=sin(cum_angles2);   
+   
 
     % Computation ------------------------------------------------------------- 
 
     fprintf('\nComputing WLC 3D Distance for K=%u links, for N=%u iterations',K_local,N)
     for ii=1:N %loop over N iterations(generate N independent runs)
-        tic %start a clock for each run 
-        for jj=1:K_local-1 %compute K segments %FIX                      
-            %find alpha and beta of PREVIOUS iteration
-            alpha_t=acos(tangents(3,jj,ii)); %arccos(t_z)       
-            beta_t=atan2(tangents(2,jj,ii),tangents(1,jj,ii));%arctan(t_y/t_x)
-            ortho_1=[cos(alpha_t)*cos(beta_t);cos(alpha_t)*sin(beta_t);-sin(alpha_t)];
-            ortho_2=[-sin(beta_t);cos(beta_t);0];
-
-            %calculate coefficients
-            %TODO: i feel like we can do some clever rewriting to reduce
-            %computational cost here (trigonometry???)
-            norm_factor=sqrt(1-(sin(rand_angles(1,jj,ii))*sin(rand_angles(2,jj,ii)))^2);
-            c_t=(cos(rand_angles(1,jj,ii))*cos(rand_angles(2,jj,ii)))/norm_factor;
-            c_1=(sin(rand_angles(1,jj,ii))*cos(rand_angles(2,jj,ii)))/norm_factor;
-            c_2=(cos(rand_angles(1,jj,ii))*sin(rand_angles(2,jj,ii)))/norm_factor; 
-
-            %calculate the new tangent vector (3D)
-            tangents(:,jj+1,ii)=c_t*tangents(:,jj,ii)+c_1*ortho_1+c_2*ortho_2;
-        end
-        
+        tic %start a clock for each run        
+             
+        %compute tangents
+        factor_cont=(sqrt(1-(sin_test_1(:,ii).*sin_test_2(:,ii)).^2));
+        tangents(1,:,ii)=sin_test_1(:,ii).*cos_test_2(:,ii)./factor_cont;
+        tangents(2,:,ii)=cos_test_1(:,ii).*sin_test_2(:,ii)./factor_cont;
+        tangents(3,:,ii)=cos_test_1(:,ii).*cos_test_2(:,ii)./factor_cont;
+       
         %update Locations (fast method)
         location(:,:,ii)=cumsum(tangents(:,:,ii)*length_link,2); 
         
@@ -92,7 +86,7 @@ for pp=1:P
 
     %calculate theoretical distance
     theoretical_dist=2*length_persist*length_chain-2*length_persist^2*...
-        (1-exp(-length_chain/length_persist));
+        (1-exp(-length_chain/(length_persist)));
     theoretical_approx=2*length_persist*length_chain-2*length_persist^2;
     
     %calculate a percentage error using |new-old|/old 
@@ -101,26 +95,40 @@ for pp=1:P
     difference_percent=100*(abs(mean(distances(:,end))-...
         theoretical_dist(end))/theoretical_dist(end));
 end
+
+
 %signaling computation is finished
 fprintf('\n>%u Configurations each with %u iterations completed, Computation finished',P,N)
 
 % Displaying Results / PostProcessing -------------------------------------
 
-%Plotting Debugging plots
+%% Plotting 
 
 %<<<Keep in mind the figures only display the LAST iteration over P (last
 %configuration!>>>
 if enable_debug_plots
     figure
     subplot(2,2,1)    
-    histogram(rand_angles(1,:,:))
+    histogram(rand_angles1,[-0.4 -0.4:0.02:0.4 0.4])
     title('[Task3]Distribution of angles (angle1)')
     xlabel('Angle')
     ylabel('Frequency')
+    yyaxis right
+   ylabel('probability')
+   range_hist=-0.4:0.02:0.4;
+   plot(range_hist,normpdf(range_hist,0,sqrt(length_link/length_persist)))
+   legend('Computed Angles','Theoretical Distribution')
+   
     subplot(2,2,2)
-    histogram(rand_angles(2,:,:))
+    histogram(rand_angles2,[-0.4 -0.4:0.02:0.4 0.4])
     title('[Task3]Distribution of angles (angle2)')
     xlabel('Angle')
+    yyaxis right
+   ylabel('probability')
+   range_hist=-0.4:0.02:0.4;
+   plot(range_hist,normpdf(range_hist,0,sqrt(length_link/length_persist)))
+   legend('Computed Angles','Theoretical Distribution')
+    
     ylabel('Frequency')
     subplot(2,2,3)
     plot(distances)
@@ -145,7 +153,7 @@ if enable_plots
                 [],linspace(1,K(end),K(end)),'filled')
             hold on
         end
-        title(sprintf('WLC plots for the first %u iterations',plot_wlc_number))
+        title(sprintf('[Task 3]WLC plots for the first %u iterations',min([N 100])))
         xlabel('X position [nm]')
         ylabel('Y position [nm]')
         zlabel('Z position [nm]')
@@ -154,7 +162,7 @@ if enable_plots
             plot3(location(1,:,ii),location(2,:,ii),location(3,:,ii))
             hold on
         end
-        title(sprintf('WLC plots for the first %u iterations',plot_wlc_number))
+        title(sprintf('[Task 3]WLC plots for the first %u iterations',min([N 100])))
         grid on
         xlabel('X position [nm]')
         ylabel('Y position [nm]')
@@ -164,7 +172,7 @@ if enable_plots
     subplot(1,2,2)
     scatter3(location(1,:,1),location(2,:,1),location(3,:,1),...
         [],linspace(1,K(end),K(end)),'filled')
-    title('WLC plot for the first iteration (single WLC)')
+    title('[Task 3]WLC plot for the first iteration (single WLC)')
     xlabel('X position [nm]')
     ylabel('Y position [nm]')
     zlabel('Z position [nm]')
@@ -175,11 +183,14 @@ end
 if enable_error_plot
     %%make non-for loop? more elegant, no need for performance
     %%TODO: can you check this? it seems okay now, but id like to have someone
-    %%check this
+  
+    clear error_chain
     for ii=1:P    
         error_chain(ii)=sqrt((mean(distances(:,ii).^2)-mean(distances(:,ii))^2)/N);
         %no need for preallocation here
     end   
+   
+    
     figure
     errorbar(length_chain,mean(distances,1),error_chain);
     hold on
@@ -187,7 +198,7 @@ if enable_error_plot
     hold on
     plot(length_chain,theoretical_approx)
     hold off
-    title(sprintf('Length of chain versus the squared end-to-end distance [3D]; N=%u',N))
+    title(sprintf('[Task 3]Length of chain versus the squared end-to-end distance [3D]; N=%u',N))
     xlabel('Length of chain [nm]')
     ylabel('squared end to end distance [nm^2]')
     legend('Monte Carlo','Theoretical Values','Theoretical Values (using approximation)')
